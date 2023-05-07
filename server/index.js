e41a61f486c487a321ed37dc92f21f46a8f6d91a
sadik312 <sadikislam46@gmail.com>
2023-05-07 21:58:40 +0100

    created backend routes

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
+import userRoutes from "./routes/users.js"
+import { verifyToken } from "./middleware/auth.js";
 
 // Middleware and package configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -36,8 +38,12 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+/* Routes with files */
+app.post("/auth/register", upload.single("picture"), verifyToken, register);
+
 /* Routes */
-app.post("/auth/register", upload.single("picture"), register);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /* Mongoose Setup */
 const PORT = process.env.PORT || 3001;
